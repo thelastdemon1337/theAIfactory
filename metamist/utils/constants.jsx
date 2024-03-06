a78eb4bb733abb6d75a2ca9499bb29c6f32cf1b4
admin <admin@admins-iMac-3.local>
2024-03-06 17:02:51 +0530

    ngrok update march

--- metamist/utils/constants.jsx
+++ metamist/utils/constants.jsx
@@ -1,5 +1,5 @@
 // export const apiGateway = "http://localhost:3500"
-export const apiGateway = "https://9e61-49-249-163-201.ngrok-free.app";
+export const apiGateway = "https://2f80-49-249-163-201.ngrok-free.app";
 export const config = {
   headers: {
     "ngrok-skip-browser-warning": true,
